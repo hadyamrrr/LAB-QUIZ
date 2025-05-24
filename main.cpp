e270e95fd2b2d271712cc12cea9dc72600728f58
CSC <csc@miu-net.local>
2025-05-24 11:27:22 +0300

    second branch

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespaace std;
+int main()
+{
+cout<<"second commit";
+cout<<"first commit";
+cout<<"first commit";
+cout<<"second commit";
+cout<<"first commit";
+cout<<"first commit";
+cout<<"second commit";
+cout<<"first commit";
+cout<<"first commit";
+cout<<"second commit";
+cout<<"first commit";
+cout<<"first commit";
+cout<<"second commit";
+cout<<"first commit";
+cout<<"first commit";
+cout<<"second commit";
+cout<<"first commit";
+cout<<"first commit";
+cout<<"second commit";
+cout<<"first commit";
+}
